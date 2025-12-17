9270f7ae5794224fa142691c15430c1a5ad1a3d1
수안 <rla.swan@ewhain.net>
2025-12-17 20:29:19 +0900

    comment 추가

--- my-app/src/chapter_05/CommentList.jsx
+++ my-app/src/chapter_05/CommentList.jsx
@@ -5,6 +5,7 @@ function CommentList(props){
     return(
         <div>
             <Comment name = {"김수안"} comment = {"안녕하세요, 김수안입니다."}/>
+            <Comment name = {"김유정"} comment = {"리액트 재밌어요🎶"} />
         </div>
     );
 }
