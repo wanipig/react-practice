294bb8cb28e35c1b0b5fb5abaf5740846d10b019
수안 <rla.swan@ewhain.net>
2025-12-17 20:50:35 +0900

    배열 만들어 댓글 데이터 담고 있는 객체 넣어줌

--- my-app/src/chapter_05/CommentList.jsx
+++ my-app/src/chapter_05/CommentList.jsx
@@ -1,11 +1,29 @@
 import React from "react"
 import Comment from "./Comment";
 
+const comments = [
+    {
+        name: "김수안",
+        comment: "안녕하세요, 김수안입니다.",
+    },
+    {
+        name: "김유정",
+        comment: "리액트 재밌어요🎶",
+    },
+    {
+        name: "이로하",
+        comment: "저도 리액트 배워보고 싶어요!",
+    },
+];
+
 function CommentList(props){
     return(
         <div>
-            <Comment name = {"김수안"} comment = {"안녕하세요, 김수안입니다."}/>
-            <Comment name = {"김유정"} comment = {"리액트 재밌어요🎶"} />
+            {comments.map((comment)=>{
+                return(
+                    <Comment name = {comment.name} comment = {comment.comment} />
+                );
+            })}
         </div>
     );
 }
