4cda270ef1ad81c16acb2de59ba036dc9f814ce1
수안 <rla.swan@ewhain.net>
2025-12-18 19:41:38 +0900

    주석 추가

--- my-app/src/chapter_06/NotificationList.jsx
+++ my-app/src/chapter_06/NotificationList.jsx
@@ -31,16 +31,16 @@ class NotificationList extends React.Component{
                 const index = notifications.length;
                 notifications.push(reservedNotifications[index]);
                 this.setState({
-                    notifications: notifications,
-                });
+                    notifications: notifications, // 빈 배열을 state에 넣음
+                }); // 생성자에서는 앞으로 사용할 데이터를 state에 넣어 초기화
             } else{
                 clearInterval(timer);
             }
-        }, 1000);
+        }, 1000); // 1초마다 정해진 작업을 수행
     }
     componentWillUnmount(){
         if(timer){
-            clearInterval(timer);
+            clearInterval(timer); // 기존 타이머가 존재할 경우 제거, 언마운트 이후에 타이머가 돌아가지 않게 함
         }
     }
 
