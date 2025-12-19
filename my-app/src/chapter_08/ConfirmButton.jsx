0163ee32c41f618f9aef90d750f977b01dd0821a
수안 <rla.swan@ewhain.net>
2025-12-19 11:43:57 +0900

    함수 컴포넌트로 변경

--- my-app/src/chapter_08/ConfirmButton.jsx
+++ my-app/src/chapter_08/ConfirmButton.jsx
@@ -1,32 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
-class ConfirmButton extends React.Component{
-    constructor(props){
-        super(props);
-        
-        this.state = {
-            isConfirmed: false,
-        };
+function ConfirmButton(props){
+    const [isConfirmed, setIsConfirmed] = useState(false);
 
-        this.handleConfirm = this.handleConfirm.bind(this);
-    }
-
-    handleConfirm = ()=>{
-        this.setState((prevState)=>({
-            isConfirmed: !prevState.isConfirmed,
-        }));
-    }
-
-    render(){
-        return(
-            <button
-                onClick = {this.handleConfirm}
-                disabled = {this.state.isConfirmed}
-            >
-                {this.state.isConfirmed ? "확인됨":"확인하기"}
+    const handleConfirm = ()=>{
+        setIsConfirmed((prevConfirmed)=>!prevConfirmed);
+    };
+    
+    return(
+        <button onClick = {handleConfirm} disabled = {isConfirmed}>
+                {isConfirmed ? "확인됨":"확인하기"}
             </button>
-        );
-    }
+    );
 }
+
 
 export default ConfirmButton;
