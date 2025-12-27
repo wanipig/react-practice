366425d4a224e79dc6834d74c66da78fbd1a2b50
수안 <rla.swan@ewhain.net>
2025-12-27 18:21:29 +0900

    ProfileCard 컴포넌트 생성

---
+++ my-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import card from "./Card";
+
+function ProfileCard(props){
+    return (
+        <Card title = "Suan Kim" backgroundColor = "4ea04e">
+            <p>안녕하세요, 소플입니다.</p>
+            <p>저는 리액트를 사용해서 개발하고 있습니다.</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
