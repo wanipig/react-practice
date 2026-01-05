9a6ea934c66211c48671c5525e279bf5554e0c42
수안 <rla.swan@ewhain.net>
2026-01-05 19:31:56 +0900

    MainPage 컴포넌트 추가 및 styled-components 재설치

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -16,11 +16,12 @@ import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
 import ProfileCard from './chapter_13/ProfileCard';
 import DarkOrLight from './chapter_14/DarkOrLight';
+import MainPage from './chapter_15/MainPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DarkOrLight />
+    <MainPage />
   </React.StrictMode>
 );
 
